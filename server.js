aa9ec5a50e446acf852c693f72bcc690991911ee
carftlider <javlonr808@gmail.com>
2025-07-21 14:38:44 +0500

    update link or changes

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+// Static fayllarni 'public' papkadan servis qilish
+app.use(express.static(path.join(__dirname, 'public')));
 
+// '/' ga borsa, index.html ni yuborish
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
